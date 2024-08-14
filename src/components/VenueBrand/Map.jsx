9759e746063566daa2343cd7a1141a71935052b6
david osezua <davidosez78@gmail.com>
2024-08-14 12:12:49 +0100

    ...

--- src/components/VenueBrand/Map.jsx
+++ src/components/VenueBrand/Map.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Map.css"
+import "./Map.css";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 export default function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
