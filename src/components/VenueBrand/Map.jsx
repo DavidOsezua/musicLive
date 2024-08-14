147f3fdfda46abe7a5da9e2e7a04cbc5cf1c7baa
david osezua <davidosez78@gmail.com>
2024-08-14 12:19:13 +0100

    ..

--- src/components/VenueBrand/Map.jsx
+++ src/components/VenueBrand/Map.jsx
@@ -1,36 +1,36 @@
-import React from "react";
-import "./Map.css";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
-
-export default function Map() {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>This is a popup</Popup>
-      </Marker>
-    </MapContainer>
-  );
-}
-
 // import React from "react";
-// import GoogleMapReact from "google-map-react";
+// import "./Map.css";
+// import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
-// const Map = () => {
+// export default function Map() {
 //   return (
-//     <div className="w-full h-full">
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: "" }}
-//         defaultCenter={{ lat: 46.195042108660154, lng: 25.136718750000004 }}
-//         center={{ lat: 46.195042108660154, lng: 25.136718750000004 }}
-//         defaultZoom={14}
-//         margin={[50, 50, 50, 50]}
-//       ></GoogleMapReact>
-//     </div>
+//     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={[51.505, -0.09]}>
+//         <Popup>This is a popup</Popup>
+//       </Marker>
+//     </MapContainer>
 //   );
-// };
+// }
 
-// export default Map;
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const Map = () => {
+  return (
+    <div className="w-full h-full">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={{ lat: 46.195042108660154, lng: 25.136718750000004 }}
+        center={{ lat: 46.195042108660154, lng: 25.136718750000004 }}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
