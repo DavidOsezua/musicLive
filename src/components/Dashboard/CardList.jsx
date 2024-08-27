9f424cd027a775420fdf72736ff296e8b969bc49
david osezua <davidosez78@gmail.com>
2024-08-27 16:50:19 +0100

    still working on the admin dashboard

--- src/components/Dashboard/CardList.jsx
+++ src/components/Dashboard/CardList.jsx
@@ -5,25 +5,13 @@ import CardListItem from "./CardListItem";
 import styles from "./CardList.module.css";
 
 const CardList = ({ data }) => {
-  const [cardData, setCardData] = useState(data);
-
-  // Function to update the status of a specific item
-  const updateItemStatus = (id, newStatus) => {
-    console.log(`Updating item with id: ${id} to status: ${newStatus}`);
-    setCardData((prevData) =>
-      prevData.map((item) =>
-        item.id === id ? { ...item, status: newStatus } : item
-      )
-    );
-  };
   return (
     <div className={`${styles.cardContainer}`}>
-      {cardData.map((item, i) => (
+      {data.map((item, i) => (
         <CardListItem
           index={i}
           key={item.ID}
           item={item}
-          updateItemStatus={updateItemStatus}
           // curr={curr}
         />
       ))}
