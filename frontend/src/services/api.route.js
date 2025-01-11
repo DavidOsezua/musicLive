9b12f7faa8b9871528cbdc45dd0c4dd4396663c9
elite <ttech7633@gmail.com>
2025-01-12 00:03:27 +0100

    Change url

--- frontend/src/services/api.route.js
+++ frontend/src/services/api.route.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const URL = "https://findmelivemusic.com:3000";
-const URL = "http://localhost:8000";
+const URL = "https://findmelivemusic.com:3000";
+// const URL = "http://localhost:8000";
 export const api = axios.create({
   baseURL: URL,
 });
